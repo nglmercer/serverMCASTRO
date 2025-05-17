0b8b2ad08e2fc6d20c75215da5ea7008c393116d
melserngl <nglmercer@gmail.com>
2025-05-17 01:29:47 -0500

    Add Node adapter and global styles
    Integrated the Node adapter in the Astro configuration for middleware support. Added global and variables CSS files for consistent styling. Updated layout files to reference the new global CSS path.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,13 @@ import lit from '@astrojs/lit';
 
 import solidJs from '@astrojs/solid-js';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [lit(), solidJs()]
+  integrations: [lit(), solidJs()],
+
+  adapter: node({
+    mode: 'middleware'
+  })
 });
